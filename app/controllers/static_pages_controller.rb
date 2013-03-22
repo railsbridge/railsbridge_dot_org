97f66687f215cf64644289be5a33f13b43902ae0
Lillie Chilen <lillie.chilen@gmail.com>
2013-03-21 17:21:17 -0700

    New website for RailsBridge

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,41 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def learning
+  end
+
+  def teaching
+  end
+
+  def organizing
+  end
+
+  def hosting
+  end
+
+  def projects
+  end
+
+  def learning_workshop
+  end
+
+  def learning_resources
+  end
+
+  def about_chapters
+  end
+
+  def about_past
+  end
+
+  def about_team
+  end
+
+  def about_sponsors
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsbridgeSite2013::Application.config.secret_token = '105690213777696409fe82f1bba562be78b6cd0c07f76feda736acb2c24fa11b8904aa6cce2777f6588d3684e9e416b28540a5b0a81cb0e8b643f0b0ca0a7d4d'
