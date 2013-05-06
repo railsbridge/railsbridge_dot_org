82e1290bfaf07e01ca365181e0ad7284cb5885c1
betatetra <jane@betatetra.com>
2013-05-05 17:22:36 -0700

    JSS: Adding breadcrumbs for all the static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :display_breadcrumbs
+  
+  protected
+  
+  def display_breadcrumbs
+    true
+  end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,47 +1,76 @@
 class StaticPagesController < ApplicationController
+
   def home
+    add_breadcrumb 'Home', root_path
   end
 
   def about
+    add_breadcrumb 'Home', root_path
+    add_breadcrumb 'About', about_path
   end
 
   def learning
+    add_breadcrumb 'Home', root_path
+    add_breadcrumb 'Learning', learning_path
   end
 
   def teaching
+    add_breadcrumb 'Home', root_path
+    add_breadcrumb 'Teaching', teaching_path
   end
 
   def organizing
+    add_breadcrumb 'Home', root_path
+    add_breadcrumb 'Organizing', organizing_path
   end
 
   def hosting
+    add_breadcrumb 'Home', root_path
+    add_breadcrumb 'Hosting', hosting_path
   end
 
   def projects
-  end
-
-  def learning_workshop
+    add_breadcrumb 'Home', root_path
+    add_breadcrumb 'Projects', projects_path
   end
 
   def learning_resources
+    add_breadcrumb 'Home', root_path
+    add_breadcrumb 'Learning Resources', learning_resources_path
   end
 
   def about_chapters
+    add_breadcrumb 'Home', root_path
+    add_breadcrumb 'About', about_path
+    add_breadcrumb 'Chapters', about_chapters_path
   end
 
   def about_past
+    add_breadcrumb 'Home', root_path
+    add_breadcrumb 'About', about_path
+    add_breadcrumb 'Past Workshops', about_past_path
   end
 
   def about_team
+    add_breadcrumb 'Home', root_path
+    add_breadcrumb 'About', about_path
+    add_breadcrumb 'Team', about_team_path
   end
 
   def about_sponsors
+    add_breadcrumb 'Home', root_path
+    add_breadcrumb 'About', about_path
+    add_breadcrumb 'Sponsors', about_sponsors_path
   end
 
   def helping
+    add_breadcrumb 'Home', root_path
+    add_breadcrumb 'Helping', helping_path
   end
 
   def interest_form
+    add_breadcrumb 'Home', root_path
+    add_breadcrumb 'Interest Form', interest_form_path
   end
 
 end
