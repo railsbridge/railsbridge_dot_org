15a1e2622f6cd0da6927e6093254852c86153826
betatetra <jane@betatetra.com>
2013-05-31 08:40:20 -0700

    Helpers for creating state toggled header and top nav links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,9 +5,18 @@ module ApplicationHelper
     title.empty? ? base_title : "#{base_title} | #{title.join(' | ')}"
   end
 
+  def header_link(link_text, link_path)
+    path_regex = /^\/#{link_text.downcase}/
+    class_name = (current_page?(link_path) || request.path.match(path_regex)) ? 'selected' : ''
+    content_tag(:li, class: class_name, id: link_text.downcase) do
+      concat link_to link_text, link_path
+      concat content_tag(:span)
+    end
+  end
+
   def nav_link(link_text, link_path)
-    class_name = current_page?(link_path) ? 'active' : ''
-    content_tag(:li, :class => class_name) do
+    class_name = current_page?(link_path) ? 'selected' : ''
+    content_tag(:li, class: class_name) do
       link_to link_text, link_path
     end
   end
