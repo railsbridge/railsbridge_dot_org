b9bf206d24a10e4859359e430b9572dc89ea1538
betatetra <jane@betatetra.com>
2013-07-19 15:29:39 -0700

    Changing css class underscores to hyphens

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -2,51 +2,51 @@ $(document).ready(function(){
   // Extremely rough draft top nav functionality
 
   $('#learn').hover(function(e) {
-    $('.top_nav_links').hide();
-    $('#learning_sub_nav').show();
+    $('.top-nav-links').hide();
+    $('#learn-sub-nav').show();
 
-    $('.header_nav li').removeClass('alive');
-    $('.top_nav ul').removeClass('alive');
-    $('#learning').addClass('alive');
-    $('#learning_sub_nav').addClass('alive');
+    $('.header-nav li').removeClass('alive');
+    $('.top-nav ul').removeClass('alive');
+    $('#learn').addClass('alive');
+    $('#learn-sub-nav').addClass('alive');
   })
   $('#volunteer').hover(function(e) {
-    $('.top_nav_links').hide();
-    $('#helping_sub_nav').show();
+    $('.top-nav-links').hide();
+    $('#volunteer-sub-nav').show();
 
-    $('.header_nav li').removeClass('alive');
-    $('.top_nav ul').removeClass('alive');
-    $('#helping').addClass('alive');
-    $('#helping_sub_nav').addClass('alive');
+    $('.header-nav li').removeClass('alive');
+    $('.top-nav ul').removeClass('alive');
+    $('#volunteer').addClass('alive');
+    $('#volunteer-sub-nav').addClass('alive');
 
   })
   $('#about').hover(function(e) {
-    $('.top_nav_links').hide();
-    $('#about_sub_nav').show();
+    $('.top-nav-links').hide();
+    $('#about-sub-nav').show();
 
-    $('.header_nav li').removeClass('alive');
-    $('.top_nav ul').removeClass('alive');
+    $('.header-nav li').removeClass('alive');
+    $('.top-nav ul').removeClass('alive');
     $('#about').addClass('alive');
-    $('#about_sub_nav').addClass('alive');
+    $('#about-sub-nav').addClass('alive');
   })
 
   $('#blog').hover(function(e) {
-    $('.top_nav_links').hide();
-    $('.top_nav_links li').removeClass('alive');
-    $('.header_nav li').removeClass('alive');
+    $('.top-nav-links').hide();
+    $('.top-nav-links li').removeClass('alive');
+    $('.header-nav li').removeClass('alive');
   })
 
   $('#curriculum').hover(function(e) {
-    $('.top_nav_links').hide();
-    $('.header_nav li').removeClass('alive');
+    $('.top-nav-links').hide();
+    $('.header-nav li').removeClass('alive');
   })
 
-  $('ul.header_nav').mouseout(function(e) {
-    console.log($('.top_nav_container:hover').length)
-    if ($('.top_nav_container:hover').length === 0) {
-      $('.top_nav_links').hide();
-      $('.header_nav li').removeClass('alive');
-      $('.top_nav .selected').show();
+  $('ul.header-nav').mouseout(function(e) {
+    console.log($('.top-nav-container:hover').length)
+    if ($('.top-nav-container:hover').length === 0) {
+      $('.top-nav-links').hide();
+      $('.header-nav li').removeClass('alive');
+      $('.top-nav .selected').show();
     }
   })
 
