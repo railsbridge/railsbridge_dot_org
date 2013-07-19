76257db2068250f5ae3b11b0e4009df09c910134
betatetra <jane@betatetra.com>
2013-07-19 16:15:00 -0700

    Adding sub nav carets

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -2,52 +2,34 @@ $(document).ready(function(){
   // Extremely rough draft top nav functionality
 
   $('#learn').hover(function(e) {
-    $('.top-nav-links').hide();
-    $('#learn-sub-nav').show();
-
-    $('.header-nav li').removeClass('alive');
-    $('.top-nav ul').removeClass('alive');
+    reset();
+    $('#js-learn-sub-nav').show().addClass('alive');
     $('#learn').addClass('alive');
-    $('#learn-sub-nav').addClass('alive');
   })
+
   $('#volunteer').hover(function(e) {
-    $('.top-nav-links').hide();
-    $('#volunteer-sub-nav').show();
-
-    $('.header-nav li').removeClass('alive');
-    $('.top-nav ul').removeClass('alive');
+    reset();
+    $('#js-volunteer-sub-nav').show().addClass('alive');
     $('#volunteer').addClass('alive');
-    $('#volunteer-sub-nav').addClass('alive');
-
   })
-  $('#about').hover(function(e) {
-    $('.top-nav-links').hide();
-    $('#about-sub-nav').show();
 
-    $('.header-nav li').removeClass('alive');
-    $('.top-nav ul').removeClass('alive');
+  $('#about').hover(function(e) {
+    reset();
+    $('#js-about-sub-nav').show().addClass('alive');
     $('#about').addClass('alive');
-    $('#about-sub-nav').addClass('alive');
   })
 
   $('#blog').hover(function(e) {
-    $('.top-nav-links').hide();
-    $('.top-nav-links li').removeClass('alive');
-    $('.header-nav li').removeClass('alive');
+    reset();
   })
 
   $('#curriculum').hover(function(e) {
-    $('.top-nav-links').hide();
+    reset();
+  })
+
+  function reset() {
     $('.header-nav li').removeClass('alive');
-  })
-
-  $('ul.header-nav').mouseout(function(e) {
-    console.log($('.top-nav-container:hover').length)
-    if ($('.top-nav-container:hover').length === 0) {
-      $('.top-nav-links').hide();
-      $('.header-nav li').removeClass('alive');
-      $('.top-nav .selected').show();
-    }
-  })
-
+    $('.top-nav ul').removeClass('alive');
+    $('.top-nav-links').hide();
+  }
 });
