b7966326da5bfe773c24b452153c1752cc313657
betatetra <jane@betatetra.com>
2013-07-19 15:08:49 -0700

    Rough draft top nav functionality

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,2 +1,53 @@
 $(document).ready(function(){
+  // Extremely rough draft top nav functionality
+
+  $('#learn').hover(function(e) {
+    $('.top_nav_links').hide();
+    $('#learning_sub_nav').show();
+
+    $('.header_nav li').removeClass('alive');
+    $('.top_nav ul').removeClass('alive');
+    $('#learning').addClass('alive');
+    $('#learning_sub_nav').addClass('alive');
+  })
+  $('#volunteer').hover(function(e) {
+    $('.top_nav_links').hide();
+    $('#helping_sub_nav').show();
+
+    $('.header_nav li').removeClass('alive');
+    $('.top_nav ul').removeClass('alive');
+    $('#helping').addClass('alive');
+    $('#helping_sub_nav').addClass('alive');
+
+  })
+  $('#about').hover(function(e) {
+    $('.top_nav_links').hide();
+    $('#about_sub_nav').show();
+
+    $('.header_nav li').removeClass('alive');
+    $('.top_nav ul').removeClass('alive');
+    $('#about').addClass('alive');
+    $('#about_sub_nav').addClass('alive');
+  })
+
+  $('#blog').hover(function(e) {
+    $('.top_nav_links').hide();
+    $('.top_nav_links li').removeClass('alive');
+    $('.header_nav li').removeClass('alive');
+  })
+
+  $('#curriculum').hover(function(e) {
+    $('.top_nav_links').hide();
+    $('.header_nav li').removeClass('alive');
+  })
+
+  $('ul.header_nav').mouseout(function(e) {
+    console.log($('.top_nav_container:hover').length)
+    if ($('.top_nav_container:hover').length === 0) {
+      $('.top_nav_links').hide();
+      $('.header_nav li').removeClass('alive');
+      $('.top_nav .selected').show();
+    }
+  })
+
 });
