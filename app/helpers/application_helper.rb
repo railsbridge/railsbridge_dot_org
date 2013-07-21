339e0ef1bd480a8b60185b6db385e9d1a17138c2
betatetra <jane@betatetra.com>
2013-07-21 10:56:49 -0700

    Using symbols for paths

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,19 +5,19 @@ module ApplicationHelper
     title.empty? ? base_title : "#{base_title} | #{title.join(' | ')}"
   end
 
-  def header_link(link_text, link_path)
+  def header_link(link_text, link)
     path_regex = /^\/#{link_text.downcase}/
-    class_name = (current_page?(link_path) || request.path.match(path_regex)) ? 'selected' : ''
+    class_name = (current_page?(link) || request.path.match(path_regex)) ? 'selected' : ''
     content_tag(:li, class: class_name, id: link_text.downcase) do
-      concat link_to link_text, link_path
+      concat link_to link_text, link
       concat content_tag(:span)
     end
   end
 
-  def nav_link(link_text, link_path)
-    class_name = current_page?(link_path) ? 'selected' : ''
+  def nav_link(link_text, link)
+    class_name = current_page?(link) ? 'selected' : ''
     content_tag(:li, class: class_name) do
-      link_to link_text, link_path
+      link_to link_text, link
     end
   end
 
