13f97b332e644396b920f3fcd38ac1d6e3c1f66e
Jane Sebastian <jane@betatetra.com>
2013-09-08 13:25:12 -0700

    Add teacher advice to tests, some styling for lists

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -85,6 +85,11 @@ describe "Static pages" do
       status_code.should be 200
     end
 
+    it "has a teacher page" do
+      visit '/help/teacher-advice'
+      status_code.should be 200
+    end
+
     it "has an organize page" do
       visit '/help/organize'
       status_code.should be 200
