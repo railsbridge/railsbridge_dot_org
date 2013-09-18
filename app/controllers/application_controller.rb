f32bf45815a6009fd209ab97973cf71a4cccb5e1
Jane Sebastian <jane@betatetra.com>
2013-09-18 14:48:51 -0700

    Temporary work around for subnav - which will be removed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :assign_nav_group
-
-  @suppress_sidenav = false
+  before_filter :assign_nav_group, :assign_suppress_sidenav
 
   private
 
   def assign_nav_group
     # TODO More graceful solution for page not being in a group
-    @nav_group = ''
+    @nav_group = false
     [:learn, :help, :about].each do |group|
       @nav_group = group if request.path.match(/^\/#{group}/)
     end
+  end
+
+  def assign_suppress_sidenav(suppress = true)
+    @suppress_sidenav = suppress
   end
 
 end
--- app/controllers/logistics_controller.rb
+++ app/controllers/logistics_controller.rb
@@ -1,15 +1,12 @@
 class LogisticsController < ApplicationController
 
   def sitemap
-    @suppress_sidenav = true
   end
 
   def error_404
-    @suppress_sidenav = true
   end
 
   def error_500
-    @suppress_sidenav = true
     render layout: false
   end
 
