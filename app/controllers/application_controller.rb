8171e50baf4445e00a83ba3e0271611f7226a8dc
Jane Sebastian <jane@betatetra.com>
2013-09-18 12:01:09 -0700

    Suppressing custom errors page in development

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,19 @@ class ApplicationController < ActionController::Base
 
   @suppress_sidenav = false
 
+  unless Rails.application.config.consider_all_requests_local
+    rescue_from Exception, with: lambda { |exception| render_error(500, exception, nil) }
+    rescue_from ActionController::RoutingError, ActionController::UnknownController, ::AbstractController::ActionNotFound, ActiveRecord::RecordNotFound, with: lambda { |exception| render_error(404, exception, 'layouts/application') }
+  end
+
   private
+
+  def render_error(status, exception, layout)
+    respond_to do |format|
+      format.html { render template: "logistics/error_#{status}", layout: layout, status: status }
+      format.all { render nothing: true, status: status }
+    end
+  end
 
   def assign_nav_group
     # TODO More graceful solution for page not being in a group
