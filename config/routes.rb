709b70c2190c30ae518b7c04b569be13346a5d60
Jane Sebastian <jane@betatetra.com>
2013-09-17 19:38:50 -0700

    Switching to more restrictive GET style routes, no other changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,28 @@
 RailsbridgeSite2013::Application.routes.draw do
+
   # devise_for :admin_users, ActiveAdmin::Devise.config
   # ActiveAdmin.routes(self)
 
   root to: 'static_pages#home'
 
-  match '/about',              to: 'static_pages#about',              as: 'about'
-  match '/about/team',         to: 'static_pages#team',               as: 'team'
-  match '/about/sponsors',     to: 'static_pages#sponsors',           as: 'sponsors'
-  match '/about/chapters',     to: 'static_pages#chapters',           as: 'chapters'
-  match '/about/past-events',  to: 'static_pages#past_events',        as: 'past_events'
-  match '/about/projects',     to: 'static_pages#projects',           as: 'projects'
+  get '/about',              to: 'static_pages#about',              as: 'about'
+  get '/about/team',         to: 'static_pages#team',               as: 'team'
+  get '/about/sponsors',     to: 'static_pages#sponsors',           as: 'sponsors'
+  get '/about/chapters',     to: 'static_pages#chapters',           as: 'chapters'
+  get '/about/past-events',  to: 'static_pages#past_events',        as: 'past_events'
+  get '/about/projects',     to: 'static_pages#projects',           as: 'projects'
 
-  match '/help',               to: 'static_pages#help',               as: 'help'
-  match '/help/teach',         to: 'static_pages#teach',              as: 'teach'
-  match '/help/teacher-advice',to: 'static_pages#teacher_advice',     as: 'teacher_advice'
-  match '/help/organize',      to: 'static_pages#organize',           as: 'organize'
-  match '/help/host',          to: 'static_pages#host',               as: 'host'
-  match '/help/donor-faq',     to: 'static_pages#donor_faq',          as: 'donor_faq'
-  match '/help/interest-form', to: 'static_pages#interest_form',      as: 'interest_form'
+  get '/help',               to: 'static_pages#help',               as: 'help'
+  get '/help/teach',         to: 'static_pages#teach',              as: 'teach'
+  get '/help/teacher-advice',to: 'static_pages#teacher_advice',     as: 'teacher_advice'
+  get '/help/organize',      to: 'static_pages#organize',           as: 'organize'
+  get '/help/host',          to: 'static_pages#host',               as: 'host'
+  get '/help/donor-faq',     to: 'static_pages#donor_faq',          as: 'donor_faq'
+  get '/help/interest-form', to: 'static_pages#interest_form',      as: 'interest_form'
 
-  match '/learn',              to: 'static_pages#learn',              as: 'learn'
-  match '/learn/resources',    to: 'static_pages#learning_resources', as: 'learning_resources'
-  match '/learn/faq',          to: 'static_pages#learning_faq',       as: 'learning_faq'
-  match '/learn/events',       to: 'static_pages#events',             as: 'events'
+  get '/learn',              to: 'static_pages#learn',              as: 'learn'
+  get '/learn/resources',    to: 'static_pages#learning_resources', as: 'learning_resources'
+  get '/learn/faq',          to: 'static_pages#learning_faq',       as: 'learning_faq'
+  get '/learn/events',       to: 'static_pages#events',             as: 'events'
 
 end
