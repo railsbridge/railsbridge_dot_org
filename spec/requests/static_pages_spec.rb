62cc1baf5e9895826d437bda0bbab69d882da756
Travis Grathwell <tjgrathwell@gmail.com>
2014-11-03 23:47:23 -0800

    Try to better center homepage image with background-size: cover
    Metaprogram the heck out of static pages smoke tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,111 +1,23 @@
 require 'spec_helper'
 
+def find_routes
+  Rails.application.routes.routes.select do |route|
+    route.defaults[:controller] == 'static_pages'
+  end
+end
+
 describe "Static pages" do
-
-  describe "Home" do
-
-    it "exists" do
-      visit '/'
-      status_code.should be 200
-    end
-
+  it 'can render a bunch of static pages' do
+    # sanity check that find_routes isn't returning nothing
+    find_routes.length.should be > 10
   end
 
-  describe "About section" do
+  find_routes.each do |route|
+    route_string = route.path.spec.to_s.gsub(/\(.*?\)/, '')
 
-    it "has an overview page" do
-      visit '/about'
+    it "renders #{route_string} successfully" do
+      visit route_string
       status_code.should be 200
     end
-
-    it "has a team page" do
-      visit '/about/team'
-      status_code.should be 200
-    end
-
-    it "has a sponsors page" do
-      visit '/about/sponsors'
-      status_code.should be 200
-    end
-
-    it "has a chapters page" do
-      visit '/about/chapters'
-      status_code.should be 200
-    end
-
-    it "has a past events page" do
-      visit '/about/past-events'
-      status_code.should be 200
-    end
-
-    it "has a projects page" do
-      visit '/about/projects'
-      status_code.should be 200
-    end
-
   end
-
-  describe "Learn section" do
-
-    it "has an overview page" do
-      visit '/learn'
-      status_code.should be 200
-    end
-
-    it "has an upcoming workshops page" do
-      visit '/learn/events'
-      status_code.should be 200
-    end
-
-    it "has a faq page" do
-      visit '/learn/faq'
-      status_code.should be 200
-    end
-
-    it "has a keep learning page" do
-      visit '/learn/resources'
-      status_code.should be 200
-    end
-
-  end
-
-  describe "Help section" do
-
-    it "has an overview page" do
-      visit '/help'
-      status_code.should be 200
-    end
-
-    it "has a teach page" do
-      visit '/help/teach'
-      status_code.should be 200
-    end
-
-    it "has a teacher page" do
-      visit '/help/teacher-advice'
-      status_code.should be 200
-    end
-
-    it "has an organize page" do
-      visit '/help/organize'
-      status_code.should be 200
-    end
-
-    it "has a host page" do
-      visit '/help/host'
-      status_code.should be 200
-    end
-
-    it "has a donor faq page" do
-      visit '/help/donor-faq'
-      status_code.should be 200
-    end
-
-    it "has an interest form page" do
-      visit '/help/interest-form'
-      status_code.should be 200
-    end
-
-  end
-
 end
