bd1e3fc09722483f65fd0c6f7e66ef9c86f45274
Travis Grathwell <tjgrathwell@gmail.com>
2015-09-05 23:08:06 -0700

    Load Bridgetroll events from /events.json instead of all_events
    This should make the frontpage a lot faster.
    
    The "past events" list will still not be very fast.

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,44 +2,34 @@ require 'active_resource'
 
 class Event < ActiveResource::Base
   self.site = BRIDGETROLL_URL
-  self.element_name = "events/all_events"
+  self.element_name = "events"
   self.timeout = 10
 
   def self.sorted_events(state = nil)
-    if state == :upcoming || state == :past
-      end_of_day = DateTime.now.end_of_day
-      events = (state == :upcoming) ? upcoming_events(end_of_day) : past_events(end_of_day)
-    else
-      events = all_events
-    end
+    events = (state == :upcoming) ? upcoming_events : past_events
     sort_by_starts_at(events)
   end
 
   private
 
-  def self.all_events
+  def self.of_type(type)
     events = begin
-      all
+      where(type: type)
     rescue StandardError => e
       puts "Error fetching events from BridgeTroll: #{e.message}"
     end
     events || []
   end
 
-  def self.upcoming_events(end_of_day)
-    all_events.select { |event| DateTime.parse(event.sessions.last.starts_at) > end_of_day }
+  def self.upcoming_events
+    of_type('upcoming')
   end
 
-  def self.past_events(end_of_day)
-    all_events.select do |event|
-      DateTime.parse(event.sessions.last.starts_at) < end_of_day
-    end.select do |event|
-      event.workshop
-    end
+  def self.past_events
+    of_type('past').select { |event| event.workshop }
   end
 
   def self.sort_by_starts_at(events)
     events.sort_by { |event| event.sessions.first.starts_at.to_time }
   end
-
 end
