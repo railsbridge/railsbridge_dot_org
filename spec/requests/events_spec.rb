383b7089616ccc578419aeb64b4f2ed004c1fa0b
Travis Grathwell <tjgrathwell@gmail.com>
2016-01-20 23:44:55 -0800

    Backfill some tests that prove Events show on the page

---
+++ spec/requests/events_spec.rb
@@ -0,0 +1,69 @@
+require 'spec_helper'
+require 'active_resource'
+
+describe "rendering events list" do
+  def default_event_attributes(date)
+    {
+      workshop: true,
+      location: {
+        name: 'City Hall',
+        city: 'San Francisco',
+        state: 'California'
+      },
+      sessions: [
+        {
+          name: 'Good Session',
+          starts_at: "#{date.year}-01-13T18:00:00.000-08:00",
+          ends_at: "#{date.year}-01-13T21:00:00.000-08:00"
+        }
+      ],
+      organizers: [
+        'Alpha Beta',
+        'Gamma Zeta'
+      ]
+    }
+  end
+
+  before do
+    ActiveResource::HttpMock.respond_to do |mock|
+      mock.get "/events.json?type=past", {}, [
+        default_event_attributes(2.years.ago).merge(
+          id: 1,
+          title: 'OlderBridge',
+        ),
+        default_event_attributes(1.year.ago).merge(
+          id: 2,
+          title: 'BygoneBridge',
+        )
+      ].to_json
+
+      mock.get "/events.json?type=upcoming", {}, [
+        default_event_attributes(1.year.from_now).merge(
+          id: 3,
+          title: 'CourtBridge',
+        )
+      ].to_json
+    end
+  end
+
+  describe "the home page" do
+    it 'has a list of upcoming events' do
+      visit '/'
+      expect(page).to have_content('CourtBridge')
+    end
+  end
+
+  describe "the events page" do
+    it 'has a list of upcoming events' do
+      visit '/learn/events'
+      expect(page).to have_content('CourtBridge')
+    end
+  end
+
+  describe "the home page" do
+    it 'has a list of past events' do
+      visit '/about/past-events'
+      expect(page.all('tbody tr').length).to eq(2)
+    end
+  end
+end
